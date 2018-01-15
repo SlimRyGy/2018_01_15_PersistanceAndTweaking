933b630170055e1939896bef28e8f3351428dafb
Rygy <remy.grandry95@gmail.com>
2018-01-15 17:27:22 +0100

    eloi fabulation

--- PersistanceAndTweaking/Assets/Scripts/TravelSave.cs
+++ PersistanceAndTweaking/Assets/Scripts/TravelSave.cs
@@ -1,25 +1,57 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
 public class TravelSave : MonoBehaviour 
 {
-
     public GameObject _target;
-
+    
     private void SaveMovement()
     {
-        _positions.Add(index, _target.transform.position);
-        _rotations.Add(index, _target.transform.rotation);
-        index += 1;
+        RecordKey key = new RecordKey();
+        key._position = _target.transform.position;
+        key._rotation = _target.transform.rotation;
+        key._time = Time.deltaTime;
+        key._index += 1;
     }
 
     private void Update()
     {
         SaveMovement();
+
+
+        //JsonUtility.ToJson();
+        //Application.dataPath
+        //File.WriteAllText(,);
     }
 
-    private Dictionary<int, Vector3> _positions;
-    private Dictionary<int, Quaternion> _rotations;
-    private int index = 0;
+    private List<RecordKey> _recordList;
+}
+
+public class Records {
+
+    private List<RecordKey> _recordList;
+
+    //public RecordKey GetPosition(float when)
+    //{
+
+    //    float time = 0;
+    //    for (int i = 0; i < _recordList.Count; i++)
+    //    {
+    //        time += _recordList[i]._time;
+    //        if (time >= when)
+    //            return _recordList[i];
+
+    //    }
+    //    return null;
+
+    //}
+}
+
+public class RecordKey {
+    public float _time;
+    public int _index = 0;
+    public Vector3 _position;
+    public Quaternion _rotation;
 }
