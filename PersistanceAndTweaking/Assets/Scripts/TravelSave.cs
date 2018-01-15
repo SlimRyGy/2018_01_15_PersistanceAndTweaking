51dd4a1a097d8cf0e35c88ef06d894ce027ebc26
Rygy <remy.grandry95@gmail.com>
2018-01-15 13:05:40 +0100

    Save Positions

---
+++ PersistanceAndTweaking/Assets/Scripts/TravelSave.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TravelSave : MonoBehaviour 
+{
+
+    public GameObject _target;
+
+    private void SaveMovement()
+    {
+        _positions.Add(index, _target.transform.position);
+        _rotations.Add(index, _target.transform.rotation);
+        index += 1;
+    }
+
+    private void Update()
+    {
+        SaveMovement();
+    }
+
+    private Dictionary<int, Vector3> _positions;
+    private Dictionary<int, Quaternion> _rotations;
+    private int index = 0;
+}
