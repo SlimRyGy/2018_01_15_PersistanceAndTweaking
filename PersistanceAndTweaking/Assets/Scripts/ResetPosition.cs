ca64b623f090a90081c35fcded5b3a35fe74ced2
Rygy <remy.grandry95@gmail.com>
2018-01-15 12:55:51 +0100

    Reset Button

---
+++ PersistanceAndTweaking/Assets/Scripts/ResetPosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPosition : MonoBehaviour 
+{
+    public GameObject _object;
+
+    public void ResetPos(GameObject obj)
+    {
+        obj.transform.SetPositionAndRotation(_pos, _rota);
+    }
+
+    public void StartPos(GameObject obj)
+    {
+        _pos    = obj.transform.position;
+        _rota   = obj.transform.rotation;
+    }
+
+    private void Start()
+    {
+        StartPos(_object);
+    }
+
+    private Vector3 _pos;
+    private Quaternion _rota;
+}
